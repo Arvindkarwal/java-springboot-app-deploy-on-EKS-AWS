b61d9a369a5de780bf7d40e24d7db24f09cd2bb9
Arvindkarwal <arvindkarwal002@gmail.com>
2025-02-04 13:45:10 +0530

    comment added --

--- src/main/java/in/flowci/a_contactmanager/contactManagerApplication.java
+++ src/main/java/in/flowci/a_contactmanager/contactManagerApplication.java
@@ -14,7 +14,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 })
 public class contactManagerApplication{
     public static void main(String[] args) {
-        // code
+        // code comment --
         SpringApplication app = new SpringApplication(contactManagerApplication.class);
         app.run(args);
     }
